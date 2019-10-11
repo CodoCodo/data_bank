d0288c89c0e1564f9a616560b06960404efc4a47
Tang Dajun <tangdajun@minieye.cc>
2019-10-11 16:52:28 +0800

    Add LabelCoreDefs
    添加LabelCoreDefs，用于定义LabelCore的变量

---
+++ src/common/label_core_defs.cpp
@@ -0,0 +1,10 @@
+#include "label_core_defs.h"
+
+namespace LabelCoreDefs {
+  const std::string kMediaTableName = "medias";
+
+  std::string GetMediaTableName() {
+    return kMediaTableName;
+  }
+}
+
---
+++ src/common/label_core_defs.h
@@ -0,0 +1,10 @@
+#ifndef _LABEL_CORE_DEFS_H
+#define _LABEL_CORE_DEFS_H
+
+#include <string>
+
+namespace LabelCoreDefs {
+  std::string GetMediaTableName();
+}
+
+#endif //_LABEL_CORE_DEFS_H
